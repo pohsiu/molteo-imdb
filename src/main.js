4f106e0b90f2f6da1927d0fa0bfe3ffaea66cca1
Louis Hsiao <louis.hsiao@d8ai.com>
2020-02-08 21:15:09 +0800

    feat: add vuetify module

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import App from './App.vue';
 import store from './store';
+import vuetify from './plugins/vuetify';
 
 Vue.use(Vuex);
 Vue.config.productionTip = false;
 
 new Vue({
   render: (h) => h(App),
+  vuetify,
   store,
 }).$mount('#app');
