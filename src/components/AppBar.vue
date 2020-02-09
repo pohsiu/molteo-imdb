166881ac6341604ee7514681ad1ab63c50586eee
Louis Hsiao <louis.hsiao@d8ai.com>
2020-02-09 17:57:19 +0800

    feat: add appbar && drawer

---
+++ src/components/AppBar.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-app-bar
+    app
+    clipped-left
+    color="red"
+    dense
+  >
+    <v-app-bar-nav-icon @click.stop="clickMenuIcon" />
+    <v-toolbar-title class="mr-12 align-center">
+      <span class="title">EXPLORE MOVIE</span>
+    </v-toolbar-title>
+    <v-spacer />
+    <v-row
+      align="center"
+      style="max-width: 650px"
+    >
+      <v-text-field
+        @click:append="onSearch"
+        @keydown="onKeydownInput"
+        placeholder="Search..."
+        single-line
+        append-icon="search"
+        color="white"
+        hide-details
+      />
+    </v-row>
+  </v-app-bar>
+</template>
+
+<script>
+import { mapMutations, mapActions } from 'vuex';
+import { GET_TYPES, ACTIONS } from '../constants';
+
+export default {
+  name: 'AppBar',
+  methods: {
+    ...mapMutations({
+      clickMenuIcon: 'setDrawerOpen',
+    }),
+    ...mapActions([
+      ACTIONS.GET_MOVIES,
+    ]),
+    onSearch() {
+      this[ACTIONS.GET_MOVIES]({
+        params: {
+          query: this.searchValue,
+        },
+        type: GET_TYPES.SEARCH,
+      });
+    },
+    onKeydownInput(event) {
+      this.searchValue = event.target.value;
+      if (event.target.value && event.keyCode === 13) {
+        this.onSearch();
+      }
+    },
+  },
+};
+</script>
---
+++ src/components/Drawer.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-navigation-drawer
+    :value="drawerOpen"
+    app
+    clipped
+  >
+    <v-list dense>
+      <v-list-item
+        v-for="item in items"
+        :key="item.text"
+        link
+      >
+        <v-list-item-action>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title>
+            {{ item.text }}
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-subheader class="mt-4 grey--text text--darken-1">SUBSCRIPTIONS</v-subheader>
+      <v-list-item
+        class="mt-4"
+        link
+      >
+        <v-list-item-action>
+          <v-icon color="grey darken-1">mdi-plus-circle-outline</v-icon>
+        </v-list-item-action>
+        <v-list-item-title class="grey--text text--darken-1">Browse Channels</v-list-item-title>
+      </v-list-item>
+      <v-list-item link>
+        <v-list-item-action>
+          <v-icon color="grey darken-1">mdi-settings</v-icon>
+        </v-list-item-action>
+        <v-list-item-title class="grey--text text--darken-1">
+          Manage Subscriptions
+        </v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  name: 'Drawer',
+  data: () => ({
+    items: [
+      { icon: 'home', text: 'Home' },
+      { icon: 'history', text: 'History' },
+    ],
+  }),
+  computed: mapState({
+    drawerOpen: (state) => state.app.drawerOpen,
+  }),
+};
+</script>
