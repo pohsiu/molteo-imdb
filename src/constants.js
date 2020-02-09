08ab8c6724ee1be055ca696d80de963676f08406
Louis Hsiao <louis.hsiao@d8ai.com>
2020-02-09 12:11:20 +0800

    chore: add more actions

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,30 @@
 export const MUTATE_ACTIONS = {
   UPDATE_MOVIES: 'UPDATE_MOVIES',
+  UPDATE_SELECTED_MOVIE: 'UPDATE_SELECTED_MOVIE',
 };
 
 export const ACTIONS = {
   GET_MOVIES: 'GET_MOVIES',
+  GET_MOVIE_DETIAL: 'GET_MOVIE_DETIAL',
+  FIND_MOVIES: 'FIND_MOVIES',
+};
+
+export const GET_TYPES = {
+  DISCOVER: 'discover',
+  SEARCH: 'search',
+};
+
+export const defaultConfigMap = {
+  [GET_TYPES.DISCOVER]: {
+    language: 'en-US',
+    sort_by: 'popularity.desc',
+    include_adult: 'false',
+    include_video: 'false',
+    page: 1,
+  },
+  [GET_TYPES.SEARCH]: {
+    language: 'en-US',
+    include_adult: 'false',
+    page: 1,
+  },
 };
