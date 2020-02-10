2f6b9f29078d5fd71dcb3cf204b189c003c5310a
Louis Hsiao <louis.hsiao@d8ai.com>
2020-02-10 20:52:19 +0800

    feat: modify rendering

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@
         class="overflow-y-auto"
         max-height="600"
       >
+        <!-- TODO: might compose router feature -->
         <div v-if="focusNaviPage === 'HOME'">
           <MovieSection
             v-if="searchs.length > 0"
@@ -15,6 +16,10 @@
             v-bind:movies="searchs"
           />
           <MovieSection title="DISCOVER" v-bind:movies="discovers" />
+        </div>
+        <div v-if="focusNaviPage === 'HISTORY'">
+          <SearchHistroy v-bind:records="searchMvRecords" />
+          <div class="display-2 py-6 pl-4">View History: </div>
         </div>
       </v-sheet>
     </v-content>
@@ -26,6 +31,7 @@ import { mapGetters } from 'vuex';
 import Drawer from './components/Drawer.vue';
 import AppBar from './components/AppBar.vue';
 import MovieSection from './components/MovieSection.vue';
+import SearchHistroy from './components/history/SearchHistory.vue';
 import { ACTIONS, GET_TYPES, Selectors } from './constants';
 
 export default {
@@ -34,12 +40,14 @@ export default {
     Drawer,
     AppBar,
     MovieSection,
+    SearchHistroy,
   },
   computed: mapGetters({
     discovers: Selectors.selectDiscovers,
     searchs: Selectors.selectSearchs,
     currentSearch: Selectors.selectCurrentSearch,
     focusNaviPage: Selectors.selectFocusNaviPage,
+    searchMvRecords: Selectors.selectSearchedRecords,
   }),
   mounted() {
     this.$store.dispatch(ACTIONS.GET_MOVIES, {
