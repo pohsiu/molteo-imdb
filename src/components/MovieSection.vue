8d9d10f00ad9e7b65c6536cd87acea6e3cf3c9ce
Louis <louis.hsiao@d8ai.com>
2020-02-12 19:38:28 +0800

    feat: use MovieCard sync

--- src/components/MovieSection.vue
+++ src/components/MovieSection.vue
@@ -8,89 +8,29 @@
         :key="movie.id"
         :cols="4"
       >
-        <v-card>
-          <v-img
-            v-if="movie.poster_path"
-            :src="imgBaseUrl + movie.poster_path"
-            class="white--text align-end hoverable"
-            gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-            height="200px"
-          >
-            <v-card-title
-              class="headline mb-1"
-              v-text="movie.original_title">
-            </v-card-title>
-            <v-card-subtitle
-              class="white--text"
-              v-text="formatReleaseDate(movie.release_date)">
-            </v-card-subtitle>
-          </v-img>
-          <!-- TODO: load default image -->
-          <v-img
-            v-else
-            class="white--text align-end hoverable"
-            :src="'loadfailed'"
-            gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-            height="200px"
-          >
-            <v-card-title
-              class="headline mb-1"
-              v-text="movie.original_title">
-            </v-card-title>
-            <v-card-subtitle
-              class="white--text"
-              v-text="formatReleaseDate(movie.release_date)">
-            </v-card-subtitle>
-          </v-img>
-          <v-card-actions>
-            <DetailDialog
-              v-bind:onClick="() => clickMovie(movie.id)"
-              v-bind:formatReleaseDate="formatReleaseDate"
-              v-bind:imgBaseUrl="imgBaseUrl"
-              v-bind:movie="movieIdMap[movie.id]"
-            />
-          </v-card-actions>
-        </v-card>
+        <MovieCard
+          v-bind:movie="movieIdMap[movie.id]"
+          :updateMovie="true"
+        />
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
-import { mapActions, mapGetters } from 'vuex';
-import moment from 'moment';
-import DetailDialog from './DetailDialog.vue';
-import { ACTIONS, Selectors } from '../constants';
+import { mapGetters } from 'vuex';
+import MovieCard from './MovieCard.vue';
+import { Selectors } from '../constants';
 
 export default {
   name: 'MovieSection',
   props: ['title', 'movies'],
   components: {
-    DetailDialog,
-  },
-  data() {
-    return {
-      imgBaseUrl: 'https://image.tmdb.org/t/p/w500/',
-    };
+    MovieCard,
   },
   computed: mapGetters({
     movieIdMap: Selectors.selectIdMap,
   }),
-  methods: {
-    ...mapActions([
-      ACTIONS.GET_MOVIE_DETIAL,
-    ]),
-    clickMovie(id) {
-      this[ACTIONS.GET_MOVIE_DETIAL]({
-        movieId: id,
-      });
-      console.log('click Moive', id);
-    },
-    formatReleaseDate(date) {
-      if (!date) return 'unknown';
-      return moment(date).format('MMM DD, YYYY');
-    },
-  },
 };
 </script>
 
--- src/components/history/ViewHistory.vue
+++ src/components/history/ViewHistory.vue
@@ -8,48 +8,9 @@
         :key="recordId"
         :cols="4"
       >
-        <v-card>
-          <v-img
-            v-if="movieIdMap[recordId].poster_path"
-            :src="imgBaseUrl + movieIdMap[recordId].poster_path"
-            class="white--text align-end hoverable"
-            gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-            height="200px"
-          >
-            <v-card-title
-              class="headline mb-1"
-              v-text="movieIdMap[recordId].original_title">
-            </v-card-title>
-            <v-card-subtitle
-              class="white--text"
-              v-text="formatReleaseDate(movieIdMap[recordId].release_date)">
-            </v-card-subtitle>
-          </v-img>
-          <!-- TODO: load default image -->
-          <v-img
-            v-else
-            class="white--text align-end hoverable"
-            :src="'loadfailed'"
-            gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-            height="200px"
-          >
-            <v-card-title
-              class="headline mb-1"
-              v-text="movieIdMap[recordId].original_title">
-            </v-card-title>
-            <v-card-subtitle
-              class="white--text"
-              v-text="formatReleaseDate(movieIdMap[recordId].release_date)">
-            </v-card-subtitle>
-          </v-img>
-          <v-card-actions>
-            <DetailDialog
-              v-bind:formatReleaseDate="formatReleaseDate"
-              v-bind:imgBaseUrl="imgBaseUrl"
-              v-bind:movie="movieIdMap[recordId]"
-            />
-          </v-card-actions>
-        </v-card>
+        <MovieCard
+          v-bind:movie="movieIdMap[recordId]"
+        />
       </v-col>
     </v-row>
   </div>
@@ -57,29 +18,17 @@
 
 <script>
 import { mapGetters } from 'vuex';
-import moment from 'moment';
-import DetailDialog from '../DetailDialog.vue';
+import MovieCard from '../MovieCard.vue';
 import { Selectors } from '../../constants';
 
 export default {
   name: 'ViewHistory',
   props: ['records'],
-  data() {
-    return {
-      imgBaseUrl: 'https://image.tmdb.org/t/p/w500/',
-    };
-  },
   components: {
-    DetailDialog,
+    MovieCard,
   },
   computed: mapGetters({
     movieIdMap: Selectors.selectIdMap,
   }),
-  methods: {
-    formatReleaseDate(date) {
-      if (!date) return 'unknown';
-      return moment(date).format('MMM DD, YYYY');
-    },
-  },
 };
 </script>
